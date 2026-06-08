9847122eec560c764cc70a6d1069552c5cc538a0
Leif Denby <leif@denby.eu>
2026-06-08 16:31:29 +0200

    Add draft website banner

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,6 +8,18 @@ import { Footer } from "@/components/Footer";
 const Index = () => {
   return (
     <main className="min-h-screen">
+      <div className="fixed right-4 top-4 z-50 max-w-sm rounded-md bg-red-600 px-4 py-3 text-sm font-semibold leading-snug text-white shadow-lg sm:right-6 sm:top-6">
+        Hi! 👋 This is draft website to give an idea of what we are working
+        towards -
+        please{" "}
+        <a
+          href="https://github.com/mlcast-community/mlcast-website"
+          className="underline underline-offset-2 hover:text-red-100"
+        >
+          raise an issue/make a PR
+        </a>{" "}
+        with any suggestions you have ❤️⭐
+      </div>
       <Hero />
       <Mission />
       <Features />
